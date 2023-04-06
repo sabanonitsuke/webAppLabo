3f58756dc8b62a5ddfbd695bab1b2736e9e3f57e
sabanonitsuke <sabakan.nari@gmail.com>
2023-04-06 15:31:10 +0900

    secret_room作成機能

--- app/controllers/secret_rooms_controller.rb
+++ app/controllers/secret_rooms_controller.rb
@@ -2,4 +2,18 @@ class SecretRoomsController < ApplicationController
   def new
     @secret_room = SecretRoom.new
   end
+
+  def create
+    @secret_room = SecretRoom.create(secret_room_params)
+    if @secret_room.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def secret_room_params
+    params.require(:secret_room).permit(:password)
+  end
 end
