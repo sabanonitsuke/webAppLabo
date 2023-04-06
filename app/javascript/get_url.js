d57377bc53954bfd1f9eb96428a9a4571d3623d4
sabanonitsuke <sabakan.nari@gmail.com>
2023-04-06 12:47:30 +0900

    JavaScriptでURL取得

--- app/javascript/get_url.js
+++ app/javascript/get_url.js
@@ -1,6 +1,17 @@
 function post() {
-  //リクエストを送信する処理
   console.log("get_urlが読み込まれています")
+  let btnCheck = document.getElementById('btnCheck');
+
+  btnCheck.addEventListener('click', function () {
+    console.log('href:' + location.href);
+    console.log('protocol:' + location.protocol);
+    console.log('host:' + location.host);
+    console.log('hostname:' + location.hostname);
+    console.log('port:' + location.port);
+    console.log('pathname:' + location.pathname);
+    console.log('search:' + location.search);
+    console.log('hash:' + location.hash);
+  });
 };
 
 window.addEventListener('load', post);
