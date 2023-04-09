f15c97841023b31cda30518c9c203e9c8562707f
sabanonitsuke <sabakan.nari@gmail.com>
2023-04-09 16:53:16 +0900

    アイコンクリックで音が出る機能実装

---
+++ app/javascript/icon.js
@@ -0,0 +1,18 @@
+function sound_play() {
+  const whistle = document.getElementById('whistle');
+  const bell = document.getElementById('bell');
+  whistle.addEventListener('click', function () {
+    const audio = document.getElementById(whistle.classList.contains('active') ? 'audio2' : 'audio1');
+    audio.currentTime = 0;
+    audio.play();
+    whistle.classList.toggle('active');
+  });
+  bell.addEventListener('click', function () {
+    const audio = document.getElementById(bell.classList.contains('active') ? 'audio4' : 'audio3');
+    audio.currentTime = 0;
+    audio.play();
+    bell.classList.toggle('active');
+  });
+};
+
+window.addEventListener('load', sound_play);
