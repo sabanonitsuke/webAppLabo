5cd4d849f9d72c063cf4441f56e5e2cd1da055ca
sabanonitsuke <sabakan.nari@gmail.com>
2023-04-10 11:32:55 +0900

    rotateボタン追加

--- app/javascript/timer.js
+++ app/javascript/timer.js
@@ -1,34 +1,43 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const timerBtn = document.getElementById('timer-btn');
-  let timerElement = document.getElementById("timer");
-  let timeLeft = parseInt(timerElement.dataset.time);
-  let timerInterval;
-
-  timerBtn.addEventListener('click', function () {
-    const audio = document.getElementById(timerBtn.classList.contains('active') ? 'audio2' : 'audio1');
+  function ring(audio) {
     audio.currentTime = 0;
     audio.play();
-    timerBtn.classList.toggle('active');
-
-    if (timerBtn.classList.contains('active')) {
-      timerInterval = setInterval(updateTimer, 1000);
-    } else {
-      clearInterval(timerInterval);
-    }
-  });
-
+  }
 
   function updateTimer() {
-
     timeLeft -= 1;
     timerElement.textContent = timeLeft;
 
     if (timeLeft <= 0) {
       clearInterval(timerInterval);
       const audio = document.getElementById('audio3');
-      audio.currentTime = 0;
-      audio.play();
+      ring(audio);
     }
   };
+
+  const playBtn = document.getElementById('play-btn');
+  const rotateBtn = document.getElementById('rotate-btn');
+  let timerElement = document.getElementById("timer");
+  let timeLeft = parseInt(timerElement.dataset.time);
+  let timerInterval;
+
+  playBtn.addEventListener('click', function () {
+    const audio = document.getElementById(playBtn.classList.contains('active') ? 'audio2' : 'audio1');
+    ring(audio);
+    playBtn.classList.toggle('active');
+
+    if (playBtn.classList.contains('active')) {
+      timerInterval = setInterval(updateTimer, 1000);
+    } else {
+      clearInterval(timerInterval);
+    }
+  });
+
+  rotateBtn.addEventListener('mouseover', function () {
+    rotateBtn.classList.add('active');
+  });
+  rotateBtn.addEventListener('mouseout', function () {
+    rotateBtn.classList.remove('active');
+  });
 
 });
