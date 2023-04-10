ab1c76aa3c5c8a5e98474e56f5f5cf0415560ecb
sabanonitsuke <sabakan.nari@gmail.com>
2023-04-10 12:33:35 +0900

    jsを用意

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,6 +10,7 @@ require("../get_url")
 require("../check")
 require("../sound_play")
 require("../timer")
+require("../random")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
