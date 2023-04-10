3a8de34d0618ddace6354443a1464515bda06437
sabanonitsuke <sabakan.nari@gmail.com>
2023-04-10 14:56:25 +0900

    ajax投稿

--- app/javascript/random.js
+++ app/javascript/random.js
@@ -1,13 +1,30 @@
 function random() {
   console.log("ランダムが読み込まれています");
-  const memberForm = document.getElementById('member-form');
+
   const memberSubmit = document.getElementById('member-submit');
+
   memberSubmit.addEventListener('click', function (e) {
     e.preventDefault();
+    const memberForm = document.getElementById('member-form');
+    const formData = new FormData(memberForm);
     const XHR = new XMLHttpRequest();
     XHR.open("POST", "/rosters", true);
     XHR.responseType = "json";
-    XHR.send("内容");
+    XHR.send(formData);
+    XHR.onload = () => {
+      if (XHR.status != 200) {
+        alert(`Error ${XHR.status}: ${XHR.statusText}`);
+        return null;
+      };
+      const formText = document.getElementById('member-text');
+      const list = document.getElementById("list");
+      const item = XHR.response.name;
+      const html = `
+      <li>${item.name}</li>
+      `;
+      list.insertAdjacentHTML("beforeend", html);
+      formText.value = ""
+    };
   });
 };
 
