2ad188245064b186fb2d7959c6e7050c2441a853
sabanonitsuke <sabakan.nari@gmail.com>
2023-04-10 12:06:48 +0900

    rosterテーブル作成

--- db/migrate/20230410030447_create_rosters.rb
+++ db/migrate/20230410030447_create_rosters.rb
@@ -1,7 +1,7 @@
 class CreateRosters < ActiveRecord::Migration[6.0]
   def change
     create_table :rosters do |t|
-
+      t.string :name
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_09_024836) do
+ActiveRecord::Schema.define(version: 2023_04_10_030447) do
 
   create_table "halls", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "rosters", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
